0f1dfa678c3b68b468e806677e972a37a34a2dd0
Ulisses Caon <ulissescaon@gmail.com>
2021-04-09 14:48:30 -0300

    Set default stuff and bluetooth

--- config.h
+++ config.h
@@ -56,17 +56,18 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = { "firefox-esr", NULL };
-static const char *dual[] = { "dual", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *web[] = { "firefox", NULL };
+static const char *docked[] = { "docked", NULL };
+static const char *blue[] = { "blueman-manager", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_m, spawn,               {.v = dual } },
+	{ MODKEY,                       XK_m, spawn,               {.v = docked } },
+	{ MODKEY,                       XK_b, spawn,               {.v = blue } },
 	{ MODKEY,                       XK_w, spawn,               {.v = web } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_comma,  setmfact,       {.f = -0.05}  },
     { MODKEY,                       XK_period, setmfact,       {.f = +0.05}  },
 	{ MODKEY,                       XK_h,      focusstack,     {.i = +1 } },
