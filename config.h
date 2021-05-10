15abab5a753de10a7bafb7fb5cdef2a1b720a5a3
Ulisses Caon <ulissescaon@gmail.com>
2021-05-10 08:08:28 -0300

    Bring back keyboard shortcuts

--- config.h
+++ config.h
@@ -65,16 +65,20 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = { "brave-browser", NULL };
-static const char *dual[] = { "dual", NULL };
+static const char *web[] = { "brave", NULL };
+static const char *docked[] = { "docked", NULL };
+static const char *emacs[] = { "emacs", NULL };
+static const char *gmenu[] = { "gmenu", NULL };
+static const char *blueman[] = { "blueman-manager", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = blueman } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = blueman } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_m, spawn,               {.v = dual } },
+	{ MODKEY,                       XK_m, spawn,               {.v = docked } },
 	{ MODKEY,                       XK_w, spawn,               {.v = web } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_comma,  setmfact,       {.f = -0.05}  },
     { MODKEY,                       XK_period, setmfact,       {.f = +0.05}  },
 	{ MODKEY,                       XK_h,      focusstack,     {.i = +1 } },
