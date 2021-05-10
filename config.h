e17b24a49286cfe66a59ce6287f80905049578c1
Ulisses Caon <ulissescaon@gmail.com>
2021-05-10 08:00:56 -0300

    Set up alpha mode

--- config.h
+++ config.h
@@ -6,17 +6,24 @@ static const unsigned int gappx     = 5;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=9", "fontawesome:size=12" };
-static const char dmenufont[]       = "Hack Nerd Font:size=9";
+static const char *fonts[]          = { "Hack Nerd Font:size=10" };
+static const char dmenufont[]       = "Hack Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#000000";
+static const char col_cyan[]        = "#005577";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray2,  col_gray1  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -28,7 +35,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -57,42 +65,36 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = { "brave", NULL };
-static const char *docked[] = { "docked", NULL };
-static const char *blue[] = { "blueman-manager", NULL };
-static const char *bmenu[] = { "bmenu", NULL };
-static const char *gmenu[] = { "gmenu", NULL };
-static const char *emacs[] = { "emacs", NULL };
+static const char *web[] = { "brave-browser", NULL };
+static const char *dual[] = { "dual", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_e,      spawn,          {.v = emacs } },
-	{ MODKEY,                       XK_s,      spawn,          {.v = gmenu } },
-	{ MODKEY,                       XK_a,      spawn,          {.v = bmenu } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_m, spawn,               {.v = docked } },
-	{ MODKEY,                       XK_b, spawn,               {.v = blue } },
+	{ MODKEY,                       XK_m, spawn,               {.v = dual } },
 	{ MODKEY,                       XK_w, spawn,               {.v = web } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_comma,  setmfact,       {.f = -0.05}  },
     { MODKEY,                       XK_period, setmfact,       {.f = +0.05}  },
 	{ MODKEY,                       XK_h,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_l,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_s,      togglesticky,   {0} },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_j,      focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_k,      focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_j,      tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_k,      tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_J,      tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_K,      tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
